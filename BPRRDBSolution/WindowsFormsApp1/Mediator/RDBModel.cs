7a837544119317f096b259ac068fe5a15a5ac6a0
Adam Minařík <adam.minarik1@gmail.com>
2018-11-01 15:58:02 +0100

    Added RDBModel Interface

--- BPRRDBSolution/WindowsFormsApp1/Mediator/RDBModel.cs
+++ BPRRDBSolution/WindowsFormsApp1/Mediator/RDBModel.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WindowsFormsApp1.Model;
 
 namespace WindowsFormsApp1.Mediator
 {
     interface RDBModel
     {
+        //GETS
+        Project getProject(int projectID);
+        EProjectItemList getItems(int projectID, String month, String type);
+        EProjectItemList getFilterItems(int projectID, String month, String field, String fieldValue);
+        EProjectItemList getRemovedItems(int projectID, String month);
+        EProjectItemList getWatchlistItems(int projectID, String month);
+        EProjectItemList getClosedItems(int projectID, String month);
+        //INSERTS
+        void addItem(EProjectItem item, bool approval);
+        //UPDATES
+        void updateProject(Project project);
+        void updateItem(EProjectItem item, bool projectOwner);
+        void updateNewItem(EProjectItem item);
+        void updateChangedItem(EProjectItem item);
+
     }
 }
