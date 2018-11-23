df12b6b68b2b82556399f49a1aca5842d8aced2c
Adam Minařík <adam.minarik1@gmail.com>
2018-11-23 15:27:41 +0100

    ExecutionUser

--- BPRRDBSolution/WindowsFormsApp1/Model/ExecutionUser.cs
+++ BPRRDBSolution/WindowsFormsApp1/Model/ExecutionUser.cs
@@ -8,5 +8,28 @@ namespace WindowsFormsApp1.Model
 {
     class ExecutionUser : Project
     {
+        //INT
+        public int userID { get; set; }
+        public int roleID { get; set; }
+        //STRING
+        public string userName { get; set; }
+        public string firstName { get; set; }
+        public string middleName { get; set; }
+        public string lastName { get; set; }
+        public string GID { get; set; }
+        public string email { get; set; }
+
+
+        public ExecutionUser(int userID, int roleID, string userName, string firstName, string middleName, string lastName, string GID, string email)
+        {
+            this.userID = userID;
+            this.roleID = roleID;
+            this.userName = userName;
+            this.firstName = firstName;
+            this.middleName = middleName;
+            this.lastName = lastName;
+            this.GID = GID;
+            this.email = email;
+        }
     }
 }
